e1833768d226a9c344be70d8e82804ece97d4b43
penguin_03 <penguin_03@naver.com>
2023-08-08 17:51:37 +0900

    TEST

--- b16926/Main.java
+++ b16926/Main.java
@@ -4,6 +4,13 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static int dir[][] = {
+            { 0, 1 },
+            { 1, 0 },
+            { 0, -1 },
+            { -1, 0 }
+    };
+
     public static void main(String[] args) throws Exception {
         System.setIn(new FileInputStream("b16926/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,20 +34,24 @@ public class Main {
         while (idx < m / 2 || idx < n / 2) {
             int x = idx;
             int y = idx;
-            while (y < m - idx) {
-                queue[0].add(arr[x][y++]);
-            }
-            while (x < n - idx) {
-                queue[idx].add(arr[x++][y]);
-            }
-            while (y > idx) {
-                queue[0].add(arr[x][y--]);
-            }
-            while (x > idx) {
-                queue[idx].add(arr[x--][y]);
+            int dirIdx = 0;
+            queue[idx].add(arr[x][y]);
+            int size = (n - idx * 2) + (m - idx * 2) - ((n - (idx - 1) * 2) + (m - (idx - 1) * 2));
+
+            for (int i = 0; i < size; i++) {
+                int dx = x + dir[dirIdx][0];
+                int dy = y + dir[dirIdx][1];
+                if (dx < idx || dy < idx || dx >= n - idx || dy >= m - idx) {
+                    dirIdx++;
+                }
+                x = x + dir[dirIdx][0];
+                y = y + dir[dirIdx][1];
+                queue[idx].add(arr[x][y]);
             }
         }
-
+        while (!queue[0].isEmpty()) {
+            System.out.println(queue[0].poll() + " ");
+        }
         // int idx = 0;
 
     }
