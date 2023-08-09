da469fc23bee9204ad420c15ba8e900a41ed53ed
penguin_03 <penguin_03@naver.com>
2023-08-09 10:12:48 +0900

    TSET

--- Test/Main.java
+++ Test/Main.java
@@ -2,9 +2,10 @@ package Test;
 
 public class Main {
     public static void main(String[] args) {
-        int a[] = { 1, 2, 3, 4, 5, 6 };
-        int arr[][] = { { 1, 2 }, { 3, 4 } };
-        int i = 0, k = 0;
-        System.out.println(arr[i++][k++]);
+        // int a[] = { 1, 2, 3, 4, 5, 6 };
+        // int arr[][] = { { 1, 2 }, { 3, 4 } };
+        // int i = 0, k = 0;
+        // System.out.println(arr[i++][k++]);
+        System.out.println(10 % 0);
     }
 }
--- b16926/Main.java
+++ b16926/Main.java
@@ -1,4 +1,4 @@
-package b16926;
+// package b16926;
 
 import java.util.*;
 import java.io.*;
@@ -12,9 +12,9 @@ public class Main {
     };
 
     public static void main(String[] args) throws Exception {
-        System.setIn(new FileInputStream("b16926/input.txt"));
+        // System.setIn(new FileInputStream("b16926/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
         Queue<Integer> queue[] = new LinkedList[151];
         for (int i = 0; i < queue.length; i++)
@@ -26,33 +26,64 @@ public class Main {
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int k = 0; k < n; k++) {
+            for (int k = 0; k < m; k++) {
                 arr[i][k] = Integer.parseInt(st.nextToken());
             }
         }
+
         int idx = 0;
-        while (idx < m / 2 || idx < n / 2) {
-            int x = idx;
-            int y = idx;
-            int dirIdx = 0;
-            queue[idx].add(arr[x][y]);
-            int size = (n - idx * 2) + (m - idx * 2) - ((n - (idx - 1) * 2) + (m - (idx - 1) * 2));
+        while (idx < m / 2 && idx < n / 2) {
+            int size = getSize(idx, n, m);
 
-            for (int i = 0; i < size; i++) {
-                int dx = x + dir[dirIdx][0];
-                int dy = y + dir[dirIdx][1];
-                if (dx < idx || dy < idx || dx >= n - idx || dy >= m - idx) {
-                    dirIdx++;
-                }
-                x = x + dir[dirIdx][0];
-                y = y + dir[dirIdx][1];
-                queue[idx].add(arr[x][y]);
+            queue[idx].add(arr[idx][idx]);
+            get(idx, size, n, m, true, arr, queue[idx]);
+
+            for (int i = 0; i < r % size; i++)
+                queue[idx].add(queue[idx].poll());
+
+            arr[idx][idx] = queue[idx].poll();
+            get(idx, size, n, m, false, arr, queue[idx]);
+
+            idx++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int k = 0; k < m; k++) {
+                sb.append(arr[i][k]).append(" ");
             }
+            sb.append("\n");
         }
-        while (!queue[0].isEmpty()) {
-            System.out.println(queue[0].poll() + " ");
-        }
-        // int idx = 0;
-
+        System.out.print(sb.toString());
     }
+
+    static void get(int idx, int size, int n, int m, boolean type, int[][] arr, Queue<Integer> queue) {
+        int x = idx;
+        int y = idx;
+        int dirIdx = 0;
+        for (int i = 0; i < size - 1; i++) {
+            int dx = x + dir[dirIdx % 4][0];
+            int dy = y + dir[dirIdx % 4][1];
+
+            if (dx < idx || dy < idx || dx > n - idx - 1 || dy > m - idx - 1)
+                dirIdx++;
+
+            x = x + dir[dirIdx % 4][0];
+            y = y + dir[dirIdx % 4][1];
+
+            assign(queue, arr, x, y, type);
+        }
+    }
+
+    static int getSize(int idx, int n, int m) {
+        return (n - idx * 2) * (m - idx * 2) - ((n - (idx + 1) * 2) * (m - (idx + 1) * 2));
+    }
+
+    static void assign(Queue<Integer> queue, int arr[][], int x, int y, boolean type) {
+        if (type) {
+            queue.add(arr[x][y]);
+        } else {
+            arr[x][y] = queue.poll();
+        }
+    }
+
 }
