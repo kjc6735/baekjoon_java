068082d0ad22c822ca1564608645462839e68fe9
penguin_03 <penguin_03@naver.com>
2023-08-10 17:50:24 +0900

    TEST

--- b17406/Main.java
+++ b17406/Main.java
@@ -10,71 +10,92 @@ public class Main {
             { -1, 0 },
             { 0, -1 }
     };
+    static int rDir[][] = {
+            { 1, 0 },
+            { 0, 1 },
+            { -1, 0 },
+            { 0, -1 }
+    };
+
     static Queue<int[]> q = new LinkedList<>();
+    static int min;
+    static int n;
+    static int m;
+    static int tmp[][];
+    static int rotate[][];
+    static Queue<Integer> queue[];
 
     public static void main(String[] args) throws Exception {
 
         System.setIn(new FileInputStream("b17406/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        Queue<Integer> queue[] = new LinkedList[50];
+        queue = new LinkedList[50];
         for (int i = 0; i < queue.length; i++)
             queue[i] = new LinkedList<>();
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
         int r = Integer.parseInt(st.nextToken());
         int arr[][] = new int[n][m];
-        int tmp[][] = new int[n][m];
+        tmp = new int[n][m];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int k = 0; k < m; k++) {
                 arr[i][k] = Integer.parseInt(st.nextToken());
             }
         }
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < r; i++) {
-            clone(arr, tmp, n, m);
+        clone(arr, tmp, n, m);
+        rotate = new int[r][3];
 
+        for (int i = 0; i < r; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int x = Integer.parseInt(st.nextToken()) - 1;
             int y = Integer.parseInt(st.nextToken()) - 1;
             int R = Integer.parseInt(st.nextToken());
+            rotate[i][0] = x;
+            rotate[i][1] = y;
+            rotate[i][2] = R;
+        }
+        fn(0, r, new boolean[r]);
+        System.out.println(min);
+    }
 
-            while (R > 0) {
-                int xx = x - R;
-                int yy = y - R;
-                int size = getSize(R * 2 + 1, R * 2 + 1);
-                System.out.println(size + "  x " + xx + "  y" + yy + " r " + R);
-                // n,m 넣기
-                queue[R].add(tmp[xx][yy]);
+    static void rotate(int x, int y, int R, Queue<Integer> queue[], int dir[][]) {
 
-                // 아래로 돌거임
-                get(R, size, xx, yy, true, tmp, queue[R]);
+        while (R > 0) {
+            int xx = x - R;
+            int yy = y - R;
+            int size = getSize(R * 2 + 1, R * 2 + 1);
+            queue[R].add(tmp[xx][yy]);
 
-                // 맨앞 큐 뒤로 보내주기
-                queue[R].add(queue[R].poll());
+            get(R, size, xx, yy, true, tmp, queue[R], dir);
 
-                while (!q.isEmpty()) {
-                    int[] poll = q.poll();
-                    System.out.print(poll[0] + " " + poll[1] + "//");
-                }
+            queue[R].add(queue[R].poll());
 
-                // 맨 앞큐에 넣어주고
-                tmp[xx][yy] = queue[R].poll();
-                // 다음거 쭊ㄴ 넣어주기 ㅅㅂ
-                get(R, size, xx, yy, false, tmp, queue[R]);
+            tmp[xx][yy] = queue[R].poll();
+            get(R, size, xx, yy, false, tmp, queue[R], dir);
 
-                R--;
-                print(n, m, tmp);
-            }
-            System.out.println("===end---");
+            R--;
+        }
+    }
 
+    static void fn(int cnt, int max, boolean visited[]) {
+        if (cnt == max) {
             int arrMin = arrMin(tmp, n, m);
             if (min > arrMin)
                 min = arrMin;
-
+            return;
         }
 
+        for (int i = 0; i < max; i++) {
+            if (visited[i])
+                continue;
+            visited[i] = true;
+            rotate(rotate[i][0], rotate[i][1], rotate[i][2], queue, dir);
+            fn(cnt + 1, max, visited);
+            visited[i] = false;
+            rotate(rotate[i][0], rotate[i][1], rotate[i][2], queue, rDir);
+        }
     }
 
     static void clone(int arr[][], int tmp[][], int n, int m) {
@@ -113,12 +134,12 @@ public class Main {
 
     }
 
-    static void get(int r, int size, int n, int m, boolean type, int[][] arr, Queue<Integer> queue) {
+    static void get(int r, int size, int n, int m, boolean type, int[][] arr, Queue<Integer> queue, int dir[][]) {
 
         int x = n;
         int y = m;
         int dirIdx = 0;
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < size - 1; i++) {
             int dx = x + dir[dirIdx % 4][0];
             int dy = y + dir[dirIdx % 4][1];
             if (dx > (n + r * 2) || dy > (m + r * 2) || dx < n || dy < m)
@@ -126,7 +147,6 @@ public class Main {
 
             x = x + dir[dirIdx % 4][0];
             y = y + dir[dirIdx % 4][1];
-            // q.add(new int[] { x, y });
             assign(queue, arr, x, y, type);
             q.add(new int[] { x, y });
         }
