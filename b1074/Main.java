538751e5de7f8b1f1d7d0da5beb37adadb94f416
penguin_03@naver.com <penguin_03@naver.com>
2023-08-16 01:31:13 +0900

    TEST

--- b1074/Main.java
+++ b1074/Main.java
@@ -1,59 +1,42 @@
-package b1074;
+// package b1074;
 
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int dir[][] = {
-            { 0, 1 },
-            { 1, -1 },
-
-    };
     static int N, R, C;
     static int totalCnt = 0;
-
     public static void main(String[] args) throws Exception {
-        System.setIn(new FileInputStream("b1074/input.txt"));
+        // System.setIn(new FileInputStream("b1074/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String str[] = br.readLine().split(" ");
 
         N = Integer.parseInt(str[0]);
-        R = Integer.parseInt(str[1]);
-        C = Integer.parseInt(str[2]);
-        find(0, 0, (int) Math.pow(2, N));
+        R = Integer.parseInt(str[1]) ;
+        C = Integer.parseInt(str[2]) ;
+        find(R, C, N);
         System.out.println(totalCnt);
     }
 
-    static void find(int r, int c, int N) {
-        int half = N / 2;
-        if (half == 0)
+    static void find(int r, int c, int n) {
+         if (n == 0) {
             return;
-        System.out.println("=====start====");
-        System.out.println(r + " " + c + " " + half);
-        if (half > R && half > C) { // 1
-            System.out.println(1 + "사분면");
-        } else if (half > R && half < C) { // 2
-            totalCnt += (N * 2);
-            c = half + 1;
-            System.out.println(2 + "사분면");
-
-        } else if (half < R && half > C) { // 3
-            totalCnt += (N * 3);
-            r = half + 1;
-            System.out.println(3 + "사분면");
-
-        } else {// 4
-            totalCnt += (N * 4);
-            r = half + 1;
-            c = half + 1;
-            System.out.println(4 + "사분면");
-
         }
-        System.out.println(r + " " + c + " " + half);
-        System.out.println("=====end====");
-        find(r, c, half);
-
-        // find(r, c, N);
+        int pow =(int) Math.pow(2, n-1) ;
+        
+        if (pow > r && pow > c) { // 1
+        } else if (pow > r && pow <= c) { // 2
+            totalCnt += (pow*pow);
+            c -= pow ;
+        } else if (pow <= r && pow > c) { // 3
+            totalCnt += (pow*pow*2);
+            r -= pow;
+        } else {// 4
+            totalCnt += (pow*pow*3);
+            r -= pow;
+            c -= pow;
+        }
+        find(r, c, n-1);
     }
 }
